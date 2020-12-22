545dec570d37e725959305984145a02dd842379b
MeenakshiRajasekar <smile@DESKTOP-06N3KMO>
2020-12-22 11:34:54 +0530

    This is my first commit

---
+++ src/gitdemo/Test.java
@@ -0,0 +1,19 @@
+package gitdemo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String str1 = "Hello";
+		String str2 = "Hello";
+		
+		if(str1==str2){
+			System.out.println("Strings are same");
+		}
+		else{
+			System.out.println("Strings are not same");
+			
+		}
+
+	}
+
+}
